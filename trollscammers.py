8701eedaa0fde935459aca5aba375d71b759fd6c
Pipythonmc <47196755+pythonmcpi@users.noreply.github.com>
2021-12-16 20:17:57 -0800

    Troll nitro-get.xyz

--- trollscammers.py
+++ trollscammers.py
@@ -3,6 +3,8 @@ import random
 import string
 import os
 from itertools import cycle
+import time
+import math
 
 def randomString(stringLength=8):
     letters = string.ascii_lowercase
@@ -16,6 +18,10 @@ def randomToken(length = 57):
     letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     return ''.join(random.choice(letters) for i in range(length))
 
+def randomBase64(length = 1685):
+    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+/'
+    return ''.join(random.choice(charset) for i in range(length))
+
 with open("1names.txt") as f:
     numedefamilie = f.readlines()
 
@@ -26,29 +32,56 @@ with open("lnames.txt") as f:
 
 prenume = [x.strip() for x in prenume]
 
-domain = "discord-app.net"
-lumen_token = "IDpAuVfaeaTzGLpVbyhoog71da0w7WppYfe0MyfY"
-ref_token = "zq6I4NsG2TWrYCVlESDjbd4a5a8d2X14VeTz21EP2Y1Kc15dgCqQwiNsj"
+domain = "nitro-get.xyz"
+path = "/login"
+proto = "https"
+method = "POST" # Edit the requests.METHOD call too
+ref_token = "login"
+
+sends = 0
+spinner = '/-\\|' # eye candy lol
+start = time.time()
 
 while True:
 
-    numef_n = random.choice(numedefamilie)
-    prenume_n = random.choice(prenume)
+    #numef_n = random.choice(numedefamilie)
+    #prenume_n = random.choice(prenume)
 
-    username = numef_n + prenume_n
-    username = username.lower()
+    #username = numef_n + prenume_n
+    #username = username.lower()
 
-    password = randomString()
+    #password = randomString()
     
    # random_token = randomToken()
 
-    url = f'https://{domain}/login/dologin'
-    obj = {'username' : username, 'password' : password}
-    headers = {'authority' : domain, 'method' : 'POST', 'path' : '/login/dologin', 'scheme' : 'https', 'accept' : '*/*', 'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36', 'accept-encoding' : 'gzip, deflate, br', 'accept-language' : 'en-US,en;q=0.9', 'content-length' : '31', 'content-type' : 'application/x-www-form-urlencoded; charset=UTF-8', 'cookie' : f'lumen_session={lumen_token}; _TDG=6f527f8d338c0c8896253111fd986ed6; timezoneOffset=25200,0', 'origin' : f'https://{domain}', 'referer' : f'https://{domain}/{ref_token}', 'sec-fetch-dest' : 'empty', 'sec-fetch-mode' : 'cors', 'sec-fetch-site' : 'same-origin', 'x-requested-with' : 'XMLHttpRequest'} #if needed
+    url = f'{proto}://{domain}{path}'
+    #obj = {'username' : username, 'password' : password}
+    obj = randomBase64()
+    headers = {'host' : domain,
+               'origin': f"{proto}://{domain}",
+               'method' : method,
+               'path' : path,
+               'scheme' : proto,
+               'accept' : '*/*',
+               'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+               'accept-encoding' : 'gzip, deflate, br',
+               'accept-language' : 'en-US,en;q=0.9',
+               'content-length' : '1687',
+               'content-type' : 'application/json',
+               'cookie' : 'ln=english; cookies=true; session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkb21haW5QYXRoIjoiTmV3X3llYXIiLCJyZWYiOjMxMCwiYm90RGF0YSI6IjE4OC4xMjAuMjQ5LjE1NToyMDAwMiIsImlhdCI6MTYzOTcwNDQwNiwidmlldyI6dHJ1ZX0.mSkMKpeQ7KHkAPGHFsopM8qgZldJzQAjcBDswCyxeM8; timezoneOffset=-28800,0; _ga=GA1.2.14677122.1639704448; _gid=GA1.2.1181044151.1639704448',
+               'origin' : f'https://{domain}',
+               'referer' : f'https://{domain}/{ref_token}',
+               'sec-fetch-dest' : 'empty',
+               'sec-fetch-mode' : 'cors',
+               'sec-fetch-site' : 'same-origin',
+               'x-requested-with' : 'XMLHttpRequest'
+    } #if needed
     
     try:
         r = requests.post(url, data=obj, headers=headers)
     except requests.ConnectionError as e:
-        print(f"{type(e).__name__}: {e} using {username}:{password}")
+        print(f"(#{sends}) {type(e).__name__}: {e}")
     else:
-        print(f"POST {r.status_code} {r.reason} using {username}:{password}")
+        sends += 1
+        elapsed = time.time() - start
+        print(f"POST {r.status_code} {r.reason} (#{sends}) [{math.floor(elapsed // 3600)}:{'0' if elapsed // 60 < 10 else ''}{math.floor(elapsed // 60)}:{'0' if elapsed % 60 < 10 else ''}{math.floor(elapsed % 60)}] {spinner[sends % 4]}", end='\r')
